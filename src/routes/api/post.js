6150581c4b0848213aa7ae73cbfe0108dbe809e8
PatelDeep484 <deepatel2037@gmail.com>
2023-06-23 17:55:55 -0400

    docker files added

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -4,29 +4,28 @@ const { Fragment } = require('../../model/fragment');
 const createFragment = async (req, res) => {
   const { headers } = req;
   const contentType = headers['content-type'];
-
-  if (!Fragment.isSupportedType(contentType)) {
+  if (Fragment.isSupportedType(contentType) == false && Buffer.isBuffer(req.body) == false) {
     const unsupportedError = createErrorResponse(415, 'Type not supported');
-    res.status(415).json(unsupportedError);
-  }
-  const ownerId = req.user;
-  const fragment = new Fragment({ ownerId, type: contentType });
+    return res.status(415).send(unsupportedError);
+  } else {
+    //if (Fragment.isSupportedType(contentType) || Buffer.isBuffer(req.body)) {
+    const ownerId = req.user;
+    const fragment = new Fragment({ ownerId, type: contentType });
 
-  try {
-    await fragment.save();
-    await fragment.setData(req.body);
+    try {
+      await fragment.save();
+      await fragment.setData(req.body);
 
-    const apiUrl = process.env.API_URL;
-    const locationHeader = `http://${apiUrl}/v1/fragments/${fragment.id}`;
+      const apiUrl = process.env.API_URL;
+      const locationHeader = `${apiUrl}/v1/fragments/${fragment.id}`;
 
-    res.set('Location', locationHeader);
-    res.status(201).json(createSuccessResponse({ fragment }));
-  } catch (err) {
-    const errorResponse = createErrorResponse(500, 'Failed to create fragment');
-    res.status(500).json(errorResponse);
+      res.set('Location', locationHeader);
+      res.status(201).json(createSuccessResponse({ fragment }));
+    } catch (err) {
+      const errorResponse = createErrorResponse(500, 'Failed to create fragment');
+      res.status(500).json(errorResponse);
+    }
   }
 };
 
 module.exports = createFragment;
-
-//|| Buffer.isBuffer(req.body)
