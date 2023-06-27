004ef4df1c44b1eaf9743c4d68f7a5e019c7b357
PatelDeep484 <deepatel2037@gmail.com>
2023-06-26 21:29:01 -0400

    lab 6 step 21

--- tests/unit/post.test.js
+++ tests/unit/post.test.js
@@ -17,7 +17,7 @@ describe('POST /v1/fragments', () => {
     expect(res.statusCode).toBe(201);
     expect(res.body.status).toBe('ok');
   });
-  test('application/json content type are allowed', async () => {
+  test('application/json content type is allowed', async () => {
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
@@ -30,7 +30,7 @@ describe('POST /v1/fragments', () => {
     const res = await request(app)
       .post('/v1/fragments')
       .auth('user1@email.com', 'password1')
-      .set('content-type', 'image/jason')
+      .set('content-type', 'image/json')
       .send({ fragment: 'fragment' });
     expect(res.statusCode).toBe(415);
   });
