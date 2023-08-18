42423e5e8b962262bbf980e7757622e8fe6caee9
PatelDeep484 <deepatel2037@gmail.com>
2023-08-18 14:57:21 -0400

    lab 10

--- scripts/local-aws-setup.sh
+++ scripts/local-aws-setup.sh
@@ -9,6 +9,7 @@ echo "Setting AWS environment variables for LocalStack"
 echo "AWS_ACCESS_KEY_ID=test"
 export AWS_ACCESS_KEY_ID=test
 
+
 echo "AWS_SECRET_ACCESS_KEY=test"
 export AWS_SECRET_ACCESS_KEY=test
 
